d6daf0a68989115c929744c67cbfa1a5dcd5df68
seehimanshu <himanshu.thakur2812@gmail.com>
2022-10-11 00:39:57 +0530

    nodes.js

--- fs.js
+++ fs.js
@@ -1,6 +1,6 @@
 // fs->file sysrem module
-// const fs=require("fs");
-// const abc=require("../Git-demo/javascript/temp")
+const fs=require("fs");
+const abc=require("../Git-demo/javascript/temp")
 console.log(abc);
 //appendFileSync appends data into a file ,if file is not present it creates the files and then appends the data
 let res=fs.appendFileSync("f1.txt","hello i am f1 file");
