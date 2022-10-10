316253a0efc001f3c873c34e88d4d2eee4f9567d
seehimanshu <himanshu.thakur2812@gmail.com>
2022-10-11 00:05:14 +0530

    node.js

--- fs.js
+++ fs.js
@@ -1,1 +1,11 @@
-fdfvf
+// fs->file sysrem module
+const fs=require("fs");
+//appendFileSync appends data into a file ,if file is not present it creates the files and then appends the data
+let res=fs.appendFileSync("f1.txt","hello i am f1 file");
+fs.appendFileSync("f1.txt","\nyou guys are smart");
+console.log(res);
+// let data=fs.readFileSync("f1.txt");
+// console.log(data+"");
+let data =fs.readFileSync("f1.txt","utf-8");
+console.log(data);
+// console.log(typeof data);
