d90a604282c5c41eec68513bb9322966cb971b12
seehimanshu <himanshu.thakur2812@gmail.com>
2022-10-12 20:08:50 +0530

    os.js

--- os.js
+++ os.js
@@ -5,8 +5,23 @@
 
  //return information on the cpus
  let myCpuInfo=os.cpus();
- console.log(myCpuInfo);
+//  console.log(myCpuInfo);
 
  //return the host name of the operating system as a string
  let hostname=os.hostname();
- console.log(hostname);
+//  console.log(hostname);
+
+ let networkInfo=os.networkInterfaces();
+//  console.log(networkInfo);
+console.log(os.release());//return the operating system as a string
+
+console.log(os.platform());//return a string identifying the operating system platform
+ 
+console.log(os.type());//return the operating system name
+
+console.log(os.totalmem());//return the total amount of system memory in bytes as an integer
+
+console.log((os.uptime()));//return the system uptime in number of seconds
+let uptimeInMin=os.uptime()/3600;
+console.log(uptimeInMin);
+console.log(os.userInfo());//return information about the currently effective user
