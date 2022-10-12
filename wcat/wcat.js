b76cd198a64dc15014c83d00adf0e956734bc8cc
seehimanshu <himanshu.thakur2812@gmail.com>
2022-10-12 23:58:07 +0530

    wcat started

--- wcat/wcat.js
+++ wcat/wcat.js
@@ -1,17 +1,27 @@
  // 1) node wcat.js filepath => display the contents of a file in terminal
  // 2) node wcat.js filepath filepath2 filepath3 =>displays the content of all files in terminal in concatinative form in given order
+ // 3) node wcat.js -n file1 file2=>1.file1,2.file2
  // node wcat.js f1.txt
  // node wcat.js f1.txt f2.txt f3.txt
  const fs=require("fs")
  let inputArr=process.argv.slice(2);
-//  console.log(input);
+ console.log(inputArr);
+
 
 let filesArr=[];
 //placed files path in filesarr
+let optionsArr=[];
 for(let i=0;i<inputArr.length;i++){
-    filesArr.push(inputArr[i]);
+    let firstChar=inputArr[i].charAt(0);
+    if(firstChar=='-'){
+        optionsArr.push(inputArr[i]);
+    }
+    else{
+        filesArr.push(inputArr[i]);
+    }
+    
 }
-console.log("file to be read are"+filesArr);
+// console.log("file to be read are "+filesArr);
 
 //check if all the files are present
 for(let i=0;i<filesArr.length;i++)
@@ -33,3 +43,27 @@ for(let i=0;i<filesArr.length;i++){
     content +=fileContent+"\n";
 }
 console.log(content);
+
+let contentArr=content.split("\n");
+console.log(contentArr);
+
+//check if-> -s is present or not
+let isSpresent=optionsArr.includes("-s");
+if(isSpresent){
+    for(let i=1;i<contentArr.length;i++){
+        if(contentArr[i]=="" && contentArr[i-1]==""){
+            contentArr[i]=null;
+        }
+        else if(contentArr[i]=="" && contentArr[i-1]==null){
+            contentArr[i]=null;
+        }
+    }
+    console.table(contentArr);
+    let tempArr=[];
+    for(let i=0;i<contentArr.length;i++){
+        if(contentArr[i]!=null){
+            tempArr.push(contentArr[i]);
+        }
+    }
+    console.log(tempArr);
+}
