643a445810a4d2142b4c6d331bd859bbcabd0647
seehimanshu <himanshu.thakur2812@gmail.com>
2022-10-15 00:49:21 +0530

    fileorganizer completed

--- fileOrganizer/main.js
+++ fileOrganizer/main.js
@@ -1,6 +1,8 @@
 // entry point of my command line
 let helpFunc=require("./commands/help");
 // console.log(helpFunc.godha());
+let orgFunc=require("./commands/organize");
+let treeFunc=require("./commands/tree");
 let inputArr=process.argv.slice(2);
 let command = inputArr[0];
 let path=inputArr[1];
@@ -8,10 +10,12 @@ let path=inputArr[1];
 switch(command){
     case"tree":
     //call tree function
-    console.log("tree function called and executed succefully on path"+path);
+    treeFunc.tree(path);
+    // console.log("tree function called and executed succefully on path"+path);
     break;
     case"organise":
-    console.log("organise function called and executed succefully on path"+path);
+    orgFunc.organize(path);
+    // console.log("organise function called and executed succefully on path"+path);
     break;
     case"help":
     helpFunc.help();
