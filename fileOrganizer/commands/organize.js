32d7918098cc6d535915bdedba47f4d17e2fdc15
seehimanshu <himanshu.thakur2812@gmail.com>
2022-10-14 23:03:05 +0530

    file organizer completed

--- fileOrganizer/commands/organize.js
+++ fileOrganizer/commands/organize.js
@@ -28,15 +28,66 @@ function organize(srcPath){
 
         //3.scan the entire srcPath(downloads folder in this case)
     let allFiles =fs.readdirSync(srcPath);
-    //console.log(allFiles);
+    // console.log(allFiles);
 
 
         //4.traverse over all the files and classify them on the basis of their extension(.pdf,.mp3)
     for(let i=0;i<allFiles.length;i++){
-        let ext=allFiles[i].split(".")[1];
-        console.log(ext);
+        // let ext=allFiles[i].split(".")[1];
+        
+        let fullPathOfFile=path.join(srcPath,allFiles[i]);
+        console.log(fullPathOfFile);
+        //1.check if it is a file or folder
+        //lstatSync gives the information regarding the link provided
+        let isThisFile=fs.lstatSync(fullPathOfFile).isFile();//true ->if file h toh or false->agr folder h
+        console.log(allFiles[i]+"is"+isThisFile);
+        if(isThisFile){
+            //1.1 get ext name
+            let ext=path.extname(allFiles[i]).split(".")[1];//ext name returns the extension of the file
+            // console.log(ext);
+            // 1.2 get folder name from extension
+            let folderName=getFolderName(ext);
+            // 1.3 copy from src folder (srcPath) and paste in dest folder(folder_name eg:-documents,media etc)
+            copyFileToDest(srcPath,fullPathOfFile,folderName);
+        }
     }
 }
-let srcPath="/Users/himanshusengar/Desktop/Fjp-5/Node/fileOrganizer/downloads"
 
-organize(srcPath);
+function getFolderName(ext){
+
+    for(let key in types){
+        for(let i=0;i<types[key].length;i++){
+            if(types[key][i]==ext){
+                return key;
+            }
+        }
+    }
+    return "miscellaneous";
+}
+function copyFileToDest(srcPath,fullPathOfFile,folderName){
+    //1. folderName ka path banana h
+    let destFolderPath=path.join(srcPath,"organized_files",folderName);//..../downloads/organized_files/archives
+    //consloe.log(destFolderPath);
+    //2. check folder if exists,if it does not, then make folder
+
+    if(!fs.existsSync(destFolderPath)){
+        fs.mkdirSync(destFolderPath);
+    }
+    // 3.copy file from src folder to dest folder
+
+    //return the last portion of a path
+    let fileName =path.basename(fullPathOfFile);//abc.zip
+    let destFileName=path.join(destFolderPath,fileName);
+    // src    dest
+    fs.copyFileSync(fullPathOfFile,destFileName);// file name esliy dena padta h kyuki copyfunction file k andar ka content copy krta h naki puri file
+
+
+}
+
+
+// let srcPath="/Users/himanshusengar/Desktop/Fjp-5/Node/fileOrganizer/downloads"
+// organize(srcPath);
+
+module.exports={
+    organize:organize
+}
