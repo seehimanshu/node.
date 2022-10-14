b1ee629e2b1b106586fec0bbe3e8ad68494dabf8
seehimanshu <himanshu.thakur2812@gmail.com>
2022-10-15 03:05:50 +0530

    web scrapping continues

--- webScrapping/coronaDetails.js
+++ webScrapping/coronaDetails.js
@@ -1,18 +1,39 @@
+const { Cheerio } = require("cheerio");
 const request=require("request");
-//cheerio
-//cheerio parses HTML and it traverses the html so that data can be manipulated according to user's needs
+// cheerio
+// cheerio parses HTML and it traverses the html so that data can be manipulated according to user's needs
+const cheerio=require("cheerio");
 
 
 
 
 
+request("https://www.worldometers.info/coronavirus/",cb);
 
-// request("https://www.worldometers.info/coronavirus/",cb);
+function cb(err,res,body){
+    if(err){
+        console.log("error",err);
+    }
+    else{
+        handleHtml(body);
+    }
+    // console.log(res);
+    // console.log(body);
 
-// function cb(err,res,body){
-//     console.error("error",err);
-//     console.log(res);
-//     console.log(body);
+    //extract krke data html se
+}
+function handleHtml(html){
 
-//     //extract krke data html se
-// }
+    let selectTool=cheerio.load(html);
+    let coronaStats=selectTool(".maincounter-number");
+    // console.log(coronaStats.text());
+
+    let totalCases=selectTool(coronaStats[0]).text();
+    console.log("Total cases ->"+totalCases);
+
+    let totalDeaths=selectTool(coronaStats[1]).text();
+    console.log("Total Deaths ->"+totalDeaths);
+
+    let totalRecover=selectTool(coronaStats[2]).text();
+    console.log("Total recovered ->"+totalRecover);
+}
