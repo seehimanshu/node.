61b20f4ca93707766db77c060cae4551a7df81d6
seehimanshu <himanshu.thakur2812@gmail.com>
2022-10-17 02:22:01 +0530

    hackerrank continues

---
+++ Automation/hackerrank/hackerRank.js
@@ -0,0 +1,11 @@
+const puppeteer=require("puppeteer");
+
+let browserOpenPromise=puppeteer.launch({
+    headless:false,
+    defaultViewport:null,
+    args:["--start-maximized"]
+});
+
+browserOpenPromise.then(function (){
+    console.log("browser is open");
+})
