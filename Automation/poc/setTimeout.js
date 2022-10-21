912499ffd749e457b88e3b6c59be133100e3ec28
seehimanshu <himanshu.thakur2812@gmail.com>
2022-10-22 03:28:44 +0530

    setTimeout

--- Automation/poc/setTimeout.js
+++ Automation/poc/setTimeout.js
@@ -1,11 +1,34 @@
-function yolo(){
-    var a=10;
-    function cb(){
-        console.log("hello how are you");
-    }
-    setTimeout(cb,5000);
-    console.log(a);
+// // function yolo(){
+// //     var a=10;
+// //     function cb(){
+// //         console.log("hello how are you");
+// //     }
+// //     setTimeout(cb,5000);
+// //     console.log(a);
+// // }
+// // yolo();
+// // let b=100;
+// // console.log(b);
+
+// const request =require("request");
+// let url ="https://www.worldometers.info/coronavirus/"
+// function yolo(){
+//     var a=10;
+//     function cb(err,res,body){
+//         if(err){
+//             console.log(err);
+//         }
+//         else{
+//             console.log("hello how are you");
+//             console.log("statuscode is ",res.statusCode);
+//         }
+//     }
+//     request(url,cb);
+//     console.log(a);
+// }
+
+for(let i=1;i<=10;i++){
+    setTimeout(function(){
+        console.log(i);
+    },2000*i);
 }
-yolo();
-let b=100;
-console.log(b);
