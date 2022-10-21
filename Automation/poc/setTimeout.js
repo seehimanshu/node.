dac1e264b77c48df167efb4e58eccc0faad1c892
seehimanshu <himanshu.thakur2812@gmail.com>
2022-10-22 02:01:07 +0530

    setimeout

---
+++ Automation/poc/setTimeout.js
@@ -0,0 +1,11 @@
+function yolo(){
+    var a=10;
+    function cb(){
+        console.log("hello how are you");
+    }
+    setTimeout(cb,5000);
+    console.log(a);
+}
+yolo();
+let b=100;
+console.log(b);
