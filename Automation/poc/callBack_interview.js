bcd430958ffdea5c3ec18118f94e1e85dd87e443
seehimanshu <himanshu.thakur2812@gmail.com>
2022-10-24 03:01:46 +0530

    callback

--- Automation/poc/callBack_interview.js
+++ Automation/poc/callBack_interview.js
@@ -1,5 +1,5 @@
 // // read 2 files f1 and f2 by using callbacks
-// const fs=require("fs");
+const fs=require("fs");
 // fs.readFile("f1.txt",cb1);
 
 // function cb1(err,res){
@@ -21,4 +21,74 @@
 //         console.log("data printed");
 //     }
 // }
+
+//callback hell-> pyramid of doom 
+// fs.readFile("f1.txt", function cb1(err, res) {
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       console.log(res + "");  
+//       fs.readFile("f2.txt", function cb2(err, res) {
+//         if (err) {
+//           console.log(err);
+//         } else {
+//           console.log(res + "");
+//           fs.readFile("f3.txt", function cb3(err, res) {
+//             if (err) {
+//               console.log(err);
+//             } else {
+//               console.log(res + "");
+//               console.log("read all files");
+//             }
+//           })
+//         }
+//       });
+//     }
+// });
+
+// 1st way to overcome callback hell is to separate the callback functions 
+
+// fs.readFile("f1.txt", cb1);
+
+// function cb1(err,res) {
+//     if (err) {
+//         console.log(err);
+//     }
+//     else {
+//         console.log(res + "");
+//         fs.readFile("f2.txt", cb2);
+//     }
+// }
+
+// function cb2(err, res) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(res + "");
+//     fs.readFile("f3.txt", cb3);
+//   }
+// }
+
+// function cb3(err, res) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(res + "");
+//     console.log("data printed");
+//   }
+// }
+
+// q2 read 3 files f1.txt , f2.txt and f3.txt parallely using callbacks 
+
+fs.readFile("f1.txt", cb);
+fs.readFile("f2.txt", cb);
+fs.readFile("f3.txt", cb);
+
+function cb(err, res) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(res + "");
+  }
+}
 
